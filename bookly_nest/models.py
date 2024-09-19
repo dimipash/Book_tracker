a3ea1814af6852e7bb503bd08ac810b8720d5499
Dimitar Pashev <dim.pashev@gmail.com>
2024-09-19 16:39:26 +0300

    add models

--- bookly_nest/models.py
+++ bookly_nest/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
 
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     published_year = models.IntegerField()
