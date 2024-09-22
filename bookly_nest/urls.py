e3a8097ca0b94c6c32607e4c8b1bc7aa2633675e
Dimitar Pashev <dim.pashev@gmail.com>
2024-09-22 14:08:50 +0300

    add editing a book

--- bookly_nest/urls.py
+++ bookly_nest/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
 
-app_name = 'bookly_nest'
+app_name = "bookly_nest"
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('genres/', views.genres, name="genres"),
-    path('genres/<int:genre_id>', views.genre, name="genre"),
-    path('new_genre/', views.new_genre, name="new_genre"),
-    path('new_book/<int:genre_id>/', views.new_book, name="new_book"),
+    path("", views.index, name="index"),
+    path("genres/", views.genres, name="genres"),
+    path("genres/<int:genre_id>", views.genre, name="genre"),
+    path("new_genre/", views.new_genre, name="new_genre"),
+    path("new_book/<int:genre_id>/", views.new_book, name="new_book"),
+    path("edit_book/<int:book_id>/", views.edit_book, name="edit_book"),
 ]
--- bookly_nest/views.py
+++ bookly_nest/views.py
@@ -3,12 +3,14 @@ from .models import Book, Genre
 from .forms import GenreForm, BookForm
 from django.shortcuts import redirect
 
+
 def index(request):
-    books = Book.objects.all()    
+    books = Book.objects.all()
     context = {
-        'books': books,
+        "books": books,
     }
-    return render(request, 'bookly_nest/index.html', context=context)
+    return render(request, "bookly_nest/index.html", context=context)
+
 
 def genres(request):
     genres = Genre.objects.all()
@@ -17,40 +19,43 @@ def genres(request):
     }
     return render(request, "bookly_nest/genres.html", context=context)
 
+
 def genre(request, genre_id):
     genre = Genre.objects.get(pk=genre_id)
-    books = genre.book_set.order_by('-date_added')
+    books = genre.book_set.order_by("-date_added")
     context = {
         "genre": genre,
         "books": books,
     }
     return render(request, "bookly_nest/genre.html", context=context)
 
+
 def new_genre(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = GenreForm(request.POST)
         if form.is_valid():
             genre = form.save(commit=False)
             genre.save()
-            return redirect('bookly_nest:genres')
+            return redirect("bookly_nest:genres")
     else:
         form = GenreForm()
 
     context = {
         "form": form,
     }
-    
+
     return render(request, "bookly_nest/new_genre.html", context=context)
+
 
 def new_book(request, genre_id):
     genre = Genre.objects.get(pk=genre_id)
-    if request.method == 'POST':
+    if request.method == "POST":
         form = BookForm(request.POST)
         if form.is_valid():
             book = form.save(commit=False)
             book.genre = genre
             book.save()
-            return redirect('bookly_nest:genre', genre_id=genre_id)
+            return redirect("bookly_nest:genre", genre_id=genre_id)
     else:
         form = BookForm()
 
@@ -58,5 +63,28 @@ def new_book(request, genre_id):
         "form": form,
         "genre": genre,
     }
-    
+
     return render(request, "bookly_nest/new_book.html", context=context)
+
+
+def edit_book(request, book_id):
+    book = Book.objects.get(pk=book_id)
+    genre = book.genre
+
+    if request.method == "POST":
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            book = form.save(commit=False)
+            book.genre = genre
+            book.save()
+            return redirect("bookly_nest:genre", genre_id=genre.id)
+    else:
+        form = BookForm(instance=book)
+
+    context = {
+        "form": form,
+        "book": book,
+        "genre": genre,
+    }
+
+    return render(request, "bookly_nest/edit_book.html", context=context)
