7986a6a7bf7d96f8a15e558b396bc0647a18a8ef
Dimitar Pashev <dim.pashev@gmail.com>
2024-09-25 17:58:21 +0300

    fix csrf issue

--- bookly/settings.py
+++ bookly/settings.py
@@ -5,6 +5,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CSRF_TRUSTED_ORIGINS = ["https://web-production-986e0.up.railway.app/"]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
