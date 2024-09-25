5d750afdbce6ad1307a5cfb742798e9c39f06d40
Dimitar Pashev <dim.pashev@gmail.com>
2024-09-25 18:03:11 +0300

    fix csrf issue

--- bookly/settings.py
+++ bookly/settings.py
@@ -5,7 +5,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-CSRF_TRUSTED_ORIGINS = ["https://web-production-986e0.up.railway.app/"]
+CSRF_TRUSTED_ORIGINS = ["https://web-production-986e0.up.railway.app"]
 
 
 # Quick-start development settings - unsuitable for production
