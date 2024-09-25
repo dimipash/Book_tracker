8bb434cda45d267f0535574502a55e736ad7c336
Dimitar Pashev <dim.pashev@gmail.com>
2024-09-25 16:21:57 +0300

    update settings

--- bookly/settings.py
+++ bookly/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -65,12 +66,7 @@ WSGI_APPLICATION = "bookly.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = {"default": dj_database_url.config(conn_max_age=600)}
 
 
 # Password validation
