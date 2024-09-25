0116470ad77c542f63899be0c19ed9eb859cbb72
Dimitar Pashev <dim.pashev@gmail.com>
2024-09-25 18:13:08 +0300

    add postgres db

--- bookly/settings.py
+++ bookly/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -71,10 +72,11 @@ WSGI_APPLICATION = "bookly.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
